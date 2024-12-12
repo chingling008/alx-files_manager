7f4eaeae5d043b6e80f3b6dfa4964f4b801626e9
chingling008 <chinglingmadness@gmail.com>
2024-12-12 18:07:30 +0200

    problemWithUpload

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { getStatus, getStats } from '../controllers/AppController';
-
-const router = express.Router();
-router.get('/status', getStatus);
-router.get('/stats', getStats);
-
-export default router;
